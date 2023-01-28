e79f33010dd21d9145f2eb6ed040edd2d44c6e39
Josh Tripoli <jct324@lehigh.edu>
2023-01-28 11:33:54 -0500

    Navbar started

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-class Home extends React.Component {
-  render() {
-    return <h1>Hello World! This is the home page</h1>;
-  }
+const Home = () => {
+    return (
+		<div className="app">
+			<nav>
+            <h1> SkyScout </ h1>
+                {/* route to Home page */}
+                <h2><Link to="/">Home</Link></h2>
+               
+                {/* route to Statistics page */}
+                <h2><Link to="/Stats">Statistics</Link></h2>
+            
+                {/* route to Create Review page */}
+                <h2><Link to="/Create">Create Review</Link></h2>
+			</nav>
+		</div>
+	);
 }
 
 export default Home;
