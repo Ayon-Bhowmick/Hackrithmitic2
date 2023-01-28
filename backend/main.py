22528b39b162969e80786ea094c5b8689da7127c
Hua Chen <huachen724@gmail.com>
2023-01-28 09:47:46 -0500

    merge

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,6 @@ api = FastAPI()
 async def ping():
     return "pong"
 
-<<<<<<< HEAD
-=======
 @api.get("/graph/airline")
 async def get_airline_graph():
     return Response(content=image_bytes, media_type="image/png")
->>>>>>> fb6a14e849a7b24a0201fd1f668df98043d6d956
