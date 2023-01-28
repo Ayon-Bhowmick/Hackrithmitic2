fb6a14e849a7b24a0201fd1f668df98043d6d956
Ayon Bhowmick <ayb224@lehigh.edu>
2023-01-28 09:41:21 -0500

    fix

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from routes import *
+
 
 api = FastAPI()
 
 @api.get("/ping")
 async def ping():
     return "pong"
+
+@api.get("/graph/airline")
+async def get_airline_graph():
+    return Response(content=image_bytes, media_type="image/png")
