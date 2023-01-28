d9dc642972685ffb0f5a2389773be92a43930283
Ayon Bhowmick <ayb224@lehigh.edu>
2023-01-28 11:31:57 -0500

    change color of bars

--- backend/main.py
+++ backend/main.py
@@ -18,5 +18,15 @@ async def ping():
     },
     response_class=Response)
 async def get_airline_graph():
-    # return Response(content=graphs.graphAirlines(), media_type="image/png")
     return StreamingResponse(graphs.graphAirlines(), media_type="image/png")
+
+@api.get(
+    "/graph/airport",
+    responses = {
+        200: {
+            "content": {"image/png": {}}
+        }
+    },
+    response_class=Response)
+async def get_airport_graph():
+    return StreamingResponse(graphs.graphAirports(), media_type="image/png")
