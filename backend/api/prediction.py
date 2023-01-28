0c4df5191f3fd26a33119456cc68535684bbede7
Gerardo Hernandez Macoto <geh223@lehigh.edu>
2023-01-28 16:59:48 -0500

    predicition test

---
+++ backend/api/prediction.py
@@ -0,0 +1,12 @@
+from textblob import TextBlob
+# Get the text from user input
+text = input("Enter a sentence: ")
+
+# Get the sentiment of the text
+sentiment = TextBlob(text).sentiment.polarity
+
+# Print the sentiment
+if sentiment > 0:
+    print("Positive")
+else:
+    print("Negative")
