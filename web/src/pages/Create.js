e4b10ac4b8f9569834d35b49c1d3a543ffeb7094
ert224 <erick.tepan2001@gmail.com>
2023-01-28 11:59:33 -0500

    Review boxes

--- web/src/pages/Create.js
+++ web/src/pages/Create.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Home extends React.Component {
-  render() {
-    return <h1>Create Review Page</h1>;
+function InputBox() {
+  const [value, setValue] = useState('');
+
+  function handleChange(event) {
+    setValue(event.target.value);
   }
+
+  return (
+    <div>
+      <h1>
+        Submit Review
+      </h1>
+      <div>
+        <form>
+          <input placeholder='Title' type="text"></input>
+        </form>
+        <form>
+          <input placeholder='flight Number' type="text"></input>
+        </form>
+        <form>
+          <input placeholder='review' type="text"></input>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Home;
+export default InputBox;
