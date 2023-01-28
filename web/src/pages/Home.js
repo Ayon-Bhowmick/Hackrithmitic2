970373f6509ddc8087a89d9fafced2831c254389
Josh Tripoli <jct324@lehigh.edu>
2023-01-28 13:43:40 -0500

    Buttons styled

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -7,16 +7,18 @@ const Home = () => {
     return (
 		<div className="app">
 			<nav>
-            <img src={Logo} alt="SkyScout" />
+            <img  id="logo" src={Logo} alt="SkyScout" />
 
             {/* route to Home page */}
-            <h2 class="nav-btn"><Link to="/">Home</Link></h2>
+            <div>
+                <h2 class="nav-btn shadow"><Link to="/">Home</Link></h2>
+                
+                {/* route to Statistics page */}
+                <h2 class="nav-btn shadow"><Link to="/statistics">Statistics</Link></h2>
             
-            {/* route to Statistics page */}
-            <h2 class="nav-btn"><Link to="/statistics">Statistics</Link></h2>
-        
-            {/* route to Create Review page */}
-            <h2 class="nav-btn"><Link to="/create">Create Review</Link></h2>
+                {/* route to Create Review page */}
+                <h2 class="nav-btn shadow"><Link to="/create">Create Review</Link></h2>
+            </div>
 			</nav>
 		</div>
 	);
