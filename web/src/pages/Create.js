5267f99844c7885251b4ce92425b0c0f10998b93
Gerardo Hernandez Macoto <geh223@lehigh.edu>
2023-01-29 03:57:24 -0500

    updated

--- web/src/pages/Create.js
+++ web/src/pages/Create.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Logo from './images/logo.png';
 import { Link } from "react-router-dom";
 import Axios from 'axios'
 import './Home.css'
 import './CreateReview.css'
+import axios from 'axios';
 
 function InputBox() {
 
@@ -35,33 +37,65 @@ function InputBox() {
 
 
   const url = "https://sky-scout.onrender.com/postreview";
-  const [data, setData] = useState({
-    title: "",
-    flightNum: "",
-    review: "",
-    phoneNum: "",
-  })
+  // const [data, setData] = useState({
+  //   title: "",
+  //   // flight_number: "",
+  //   // message: "",
+  //   // phonenumber: "",
+  // })
+////////////////////////////////////////////////////////////////
+  const [title, setTitle] = useState();
+  const [message, setMessage] = useState();
+  const [flightnumber, setFlightNumber] = useState();
+  const [phonenumber, setPhonenumber] = useState();
 
-
-  function handle(e) {
-    const newdata = { ...data }
-    newdata[e.target.id] = e.target.value
-    setData(newdata)
-    // console.log(newdata)
-  }
-
-  function submit(e) {
+  const doFetech = async(e)=>{
     e.preventDefault();
-    Axios.post(url, {
-      title: data.name,
-      flightNum: data.flightNum,
-      review: data.review,
-      phoneNum: data.phoneNum
+    await axios.post("https://127.0.0.1:8000/postreview",{
+      title,
+      message,
+      flightnumber,
+      phonenumber,
     })
-      .then(res => {
-        console.log(res.data);
-      })
+    .then(response=>{
+      console.log(response.data);
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+
   }
+
+  let navigate = useNavigate();
+  const routeChange = ()=>{
+    let path = '/';
+    navigate(path);
+  }
+  
+
+///////////////////////////////////////////
+
+
+  // function handle(e) {
+  //   const newdata = { ...data }
+  //   newdata[e.target.id] = e.target.value
+  //   setData(newdata)
+  //   // console.log(newdata)
+  // }
+
+  // function submit(e) {
+  //   e.preventDefault();
+  //   Axios.post(url, {
+  //     title: data.name,
+  //     flight_number: data.flight_number,
+  //     message: data.message,
+  //     phone_number: data.phonenumber,
+  //   })
+  //     .then(res => {
+  //       console.log(res.data);
+  //     })
+  // }
+  
   return (
     <div>
       <nav>
@@ -85,31 +119,31 @@ function InputBox() {
           Write Your Review
         </h1>
         <div className='sub_form'>
-          <form onSubmit={(e) => { handleSubmit(e); submit(e); }}>
+          <form onSubmit={(e) => doFetech}>
             <div>
               <label>Title: <br /></label>
-              <input onChange={(e) => handle(e)} id="title" value={data.title} placeholder='Title' type="text" className='input_field Title'></input>
+              <input onChange={(e) => setTitle(e.target.value)} id="title" placeholder='Title' type="text" className='input_field Title'></input>
             </div>
             <div>
               <label>Phone Number:</label>
               <br />
-              <input onChange={(e) => { handle(e); }} id="phonenum" value={data.phonenum} placeholder='xxx-xxx-xxxx' type="text" pattern='[0-9]{3}[0-9]{3}[0-9]{4}' className='input_field Phone' maxLength={10} ></input>
+              <input onChange={(e) => setPhonenumber(e.target.value)} id="phonenum"  placeholder='xxx-xxx-xxxx' type="text" pattern='[0-9]{3}[0-9]{3}[0-9]{4}' className='input_field Phone' maxLength={10} ></input>
             </div>
             <div>
               <label>Flight Number:</label><span> *</span>
               <br />
-              <input onChange={(e) => { handle(e); setFlight(e.target.value); }} id="flightNum" value={data.flightNum} placeholder='Flight Number' type="text" pattern='[A-Z]{2}[0-9]{4}' className='input_field Flight' maxLength={6} required></input>
+              <input onChange={(e) => setFlightNumber(e.target.value)} id="flightNum"  placeholder='Flight Number' type="text" pattern='[A-Z]{2}[0-9]{4}' className='input_field Flight' maxLength={6} required></input>
               {(error === true) ?
                 <label className='error'>Flight needs to be specified! <br />Example: AA1234</label> : ""}
             </div>
             <div>
               <label>Review:</label> <span> *</span>
               <br />
-              <input onChange={(e) => { handle(e); setReview(e.target.value); }} id="review" value={data.review} placeholder='Review' type="text" className='input_field Review' required></input>
+              <input onChange={(e) => setMessage(e.target.value)} id="review"  placeholder='Review' type="text" className='input_field Review' required></input>
               {ReviewError && Review.length <= 0 ?
                 <label className='error'>Review needs to be entered!</label> : ""}
             </div>
-            <button className='btn' onChange={(e) => handleSubmit(e)} onClick={(e) => { handleSubmit(e) }}> Submit</button>
+            <button className='btn' onChange={(e) => handleSubmit(e)} onClick={(e) => { doFetech(e); routeChange(); }}> Submit</button>
             {/* {error&&Flight?"hello":"no error"} */}
           </form>
         </div>
